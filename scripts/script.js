7650407f7119572a110b23f7b8ecf3a02165366c
Davis Esmael <devdavisesmael@gmail.com>
2023-01-20 21:10:56 -0300

    (fix) : fix carousel index with swiper js

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+
+var swiper = new Swiper(".slide-clientes", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".slide-parcerias", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
